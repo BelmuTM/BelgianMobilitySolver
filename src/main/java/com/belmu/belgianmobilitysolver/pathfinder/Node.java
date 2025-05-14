1cbcc3984da29e4536f02c003e3c0bb68dfc0255
Belmu <belmuledozo@gmail.com>
2025-05-14 04:48:01 +0200

    Commented Node.java

--- src/main/java/com/belmu/belgianmobilitysolver/pathfinder/Node.java
+++ src/main/java/com/belmu/belgianmobilitysolver/pathfinder/Node.java
@@ -14,6 +14,11 @@ public class Node {
 
     public boolean isWalking;
 
+    /**
+     * Node class for the pathfinder's graph, holds the previous node,
+     * information about the stop and the trip's route, the current and previous
+     * arrival times and indicates if the last edge was traversed by walking or by transiting.
+     */
     public Node(Node previous, String stopId, Route route, long time, long previousTime, boolean isWalking) {
         this.previous = previous;
 
